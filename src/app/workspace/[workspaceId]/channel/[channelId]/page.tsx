4ec2fecded71a76542deb1baaa1312f38dd298e0
Kartik Gandhi <gandhikartik73@gmail.com>
2024-09-23 02:06:55 +0530

    create get message api endpoint

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -3,6 +3,7 @@
 import { Loader, TriangleAlert } from "lucide-react";
 
 import { useGetChannel } from "@/features/channels/api/use-get-channel";
+import { useGetMessages } from "@/features/messages/api/use-get-messages";
 
 import { useChannelId } from "@/hooks/use-channel-id";
 
@@ -12,6 +13,7 @@ import { ChatInput } from "./chat-input";
 const ChannelIdPage = () => {
     const channelId = useChannelId();
 
+    const { results } = useGetMessages({ channelId });
     const { data: channel, isLoading: channelLoading } = useGetChannel({
         id: channelId,
     });
@@ -38,7 +40,7 @@ const ChannelIdPage = () => {
     return (
         <div className="flex flex-col h-full">
             <Header title={channel.name} />
-            <div className="flex-1" />
+            <div className="flex-1">{JSON.stringify(results)}</div>
             <ChatInput placeholder={`Message # ${channel.name}`} />
         </div>
     );
