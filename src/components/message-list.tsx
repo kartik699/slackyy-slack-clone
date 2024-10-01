44929cd63657d4e1a6b3a3490a5cb07e384bc5a8
Kartik Gandhi <gandhikartik73@gmail.com>
2024-10-02 00:15:50 +0530

    implement infinite load messages

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -10,6 +10,7 @@ import { Message } from "./message";
 import { ChannelHero } from "./channel-hero";
 
 import { Id } from "../../convex/_generated/dataModel";
+import { Loader } from "lucide-react";
 
 const TIME_THRESHOLD = 5;
 
@@ -117,6 +118,31 @@ export const MessageList = ({
                         })}
                     </div>
                 ),
+            )}
+            <div
+                className="h-1"
+                ref={(el) => {
+                    if (el) {
+                        const observer = new IntersectionObserver(
+                            ([entry]) => {
+                                if (entry.isIntersecting && canLoadMore) {
+                                    loadMore();
+                                }
+                            },
+                            { threshold: 1.0 },
+                        );
+                        observer.observe(el);
+                        return () => observer.disconnect();
+                    }
+                }}
+            />
+            {isLoadingMore && (
+                <div className="text-center my-2 relative">
+                    <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+                    <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm">
+                        <Loader className="size-4 animate-spin" />
+                    </span>
+                </div>
             )}
             {variant === "channel" && channelName && channelCreationTime && (
                 <ChannelHero
