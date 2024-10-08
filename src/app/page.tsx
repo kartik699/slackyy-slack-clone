368a1f2b4d429dd1ae2803e3eea4d0da50540d34
Kartik Gandhi <gandhikartik73@gmail.com>
2024-10-09 01:29:42 +0530

    add favicon and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { Loader } from "lucide-react";
 import { useEffect, useMemo } from "react";
-
-import { UserButton } from "@/features/auth/components/user-button";
+import { useRouter } from "next/navigation";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
@@ -27,8 +26,8 @@ export default function Home() {
     }, [isLoading, workspaceId, open, setOpen, router]);
 
     return (
-        <div>
-            <UserButton />
+        <div className="h-full flex items-center justify-center">
+            <Loader className="size-6 animate-spin text-muted-foreground" />
         </div>
     );
 }
